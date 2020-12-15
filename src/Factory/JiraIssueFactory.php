f41db804cb3092e124f9ca209fe5795d1dda23e1
Lucas GERARD <lucas.gerard.web@gmail.com>
2020-12-15 13:13:33 +0100

    Record TMA issues into Elasticsearch

--- src/Factory/JiraIssueFactory.php
+++ src/Factory/JiraIssueFactory.php
@@ -9,8 +9,7 @@ use GuzzleHttp\Psr7\Uri;
 
 class JiraIssueFactory
 {
-    /** @var string */
-    private $host;
+    private string $host;
 
     public function __construct(string $host)
     {
@@ -20,22 +19,30 @@ class JiraIssueFactory
     public function create(array $issueData): JiraIssue
     {
         return new JiraIssue(
+            (int) $issueData['id'],
             $issueData['key'],
+            $issueData['fields']['summary'],
+            \is_array($issueData['fields']['customfield_10300']),
+            $issueData['fields']['priority']['name'],
+            new JiraIssueType(
+                $issueData['fields']['issuetype']['id'],
+                $issueData['fields']['issuetype']['name'],
+                $issueData['fields']['issuetype']['subtask']
+            ),
             new JiraIssueStatus(
                 $issueData['fields']['status']['id'],
                 $issueData['fields']['status']['name']
             ),
-            new JiraIssueType(
-                $issueData['fields']['issuetype']['id'],
-                $issueData['fields']['issuetype']['name']
-            ),
+            new \DateTimeImmutable($issueData['fields']['created']),
             new Uri(
                 sprintf(
                     '%s/browse/%s',
                     $this->host,
                     $issueData['key']
                 )
-            )
+            ),
+            $issueData['fields']['customfield_10005'],
+            null // $resolvedAt // TODO : get custom field
         );
     }
 }
