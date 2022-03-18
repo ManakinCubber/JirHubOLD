c318d8b828fcf219f19419717bf0a28e1747e64e
Vincent PERAUD <vincent.peraud@gmail.com>
2022-03-18 14:37:35 +0100

    lint

--- src/Controller/HerokuController.php
+++ src/Controller/HerokuController.php
@@ -3,11 +3,11 @@
 namespace App\Controller;
 
 use App\Handler\ChangelogHandler;
+use JoliCode\Slack\Api\Client as SlackClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use JoliCode\Slack\Api\Client as SlackClient;
 
 class HerokuController extends AbstractController
 {
@@ -19,13 +19,13 @@ class HerokuController extends AbstractController
 
     public function __construct(SlackClient $slack, ChangelogHandler $changelogHandler)
     {
-        $this->slack                 = $slack;
-        $this->changelogHandler      = $changelogHandler;
+        $this->slack            = $slack;
+        $this->changelogHandler = $changelogHandler;
     }
 
     /**
      * @Route("/heroku/deploy-hook", name="heroku_deploy_hook", methods={"POST"})
-     * 
+     *
      * Available post parameters are:
      * - app (ex: chronos-api-production)
      * - app_uuid
@@ -40,23 +40,23 @@ class HerokuController extends AbstractController
     public function herokuDeployHookAction(Request $request): Response
     {
         $repositoryOwner = getenv('GITHUB_REPOSITORY_OWNER');
-        $repositoryName = getenv('GITHUB_REPOSITORY_NAME');
+        $repositoryName  = getenv('GITHUB_REPOSITORY_NAME');
 
         $requestBag = $request->request;
 
-        $head = $requestBag->get('head');
+        $head      = $requestBag->get('head');
         $prev_head = $requestBag->get('prev_head');
-        $release = $requestBag->get('release');
-        $diff_url = sprintf('https://github.com/%s/%s/compare/%s...%s', $repositoryOwner, $repositoryName, $prev_head, $head);
+        $release   = $requestBag->get('release');
+        $diff_url  = sprintf('https://github.com/%s/%s/compare/%s...%s', $repositoryOwner, $repositoryName, $prev_head, $head);
 
         $commits = $this->changelogHandler->getOrderedChangelog($prev_head, $head);
 
         $this->slack->filesUpload([
-            'channels' => getenv('SLACK_CHANGELOG_CHANNEL'),
-            'content' => implode(PHP_EOL, $commits),
-            'title' => 'Changelog for release ' . $release,
-            'filename' => 'changelog_release_' . $release . '.txt',
-            'initial_comment' => sprintf('<%s|MEP Back>', $diff_url)
+            'channels'        => getenv('SLACK_CHANGELOG_CHANNEL'),
+            'content'         => implode(PHP_EOL, $commits),
+            'title'           => 'Changelog for release ' . $release,
+            'filename'        => 'changelog_release_' . $release . '.txt',
+            'initial_comment' => sprintf('<%s|MEP Back>', $diff_url),
         ]);
 
         return new Response(null, Response::HTTP_NO_CONTENT);
